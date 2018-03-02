8ce4c07791755ecc45d72136909aa1c738c26476
rivermanvr <vrgithub@riversconsulting.com>
2018-03-02 14:35:28 -0500

    input box placement/styling

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
         <header>
           <h1 className="App-title">React-16-Example #2</h1>
         </header>
-        <div><input /></div>
+        <div><input className="border input-style" /></div>
       </div>
     );
   }
