56e3d989a4855c3e9f0d9978465fabb9db83f549
rivermanvr <vrgithub@riversconsulting.com>
2018-03-02 14:42:27 -0500

    input a controlled component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    inputTxt: ''
+  }
+
+  onInputChange = (event) => {
+    const inputTxt = event.target.value;
+    this.setState({ inputTxt });
+  }
+
   render() {
+    const inputTxt = this.state.inputTxt;
     return (
       <div className="App">
         <header>
           <h1 className="App-title">React-16-Example #2</h1>
         </header>
-        <div><input className="border input-style" /></div>
+        <div><input onChange={ this.onInputChange } value={ inputTxt } className="border input-style" type="text" /></div>
       </div>
     );
   }
