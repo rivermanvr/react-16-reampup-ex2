337ba7c8ad461aae3909a26852a3b1c77faf1dbd
rivermanvr <vrgithub@riversconsulting.com>
2018-03-02 17:18:38 -0500

    fixing errors

--- src/Validation/Validation.js
+++ src/Validation/Validation.js
@@ -2,11 +2,6 @@ import React from 'react';
 
 const validation = ({ inputLen }) => {
   const messageOut = (inputLen >= 5) ? 'Text length is a valid length' : 'Text Length is too short';
-  return (
-    <div>
-      { messageOut }
-    </div>
-  );
+  return <div>{ messageOut }</div>;
 };
-
 export default validation;
